85fd1dec5063a3ee37a6b048ad165002ae8926af
Devin Austin <devin.austin@gmail.com>
2024-07-22 17:07:33 -0600

    initial commit

---
+++ src/main/java/in/stonecold/umumumum/UmumumumApplication.java
@@ -0,0 +1,13 @@
+package in.stonecold.umumumum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmumumumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmumumumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/stonecold/umumumum/UmumumumApplicationTests.java
@@ -0,0 +1,13 @@
+package in.stonecold.umumumum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmumumumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
