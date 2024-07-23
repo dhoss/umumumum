d5110f69c654985993d7e005708dc4ac5d1870ac
Devin Austin <devin.austin@gmail.com>
2024-07-22 18:49:38 -0600

    trying to get console output coloring working again

--- src/test/java/in/stonecold/umumumum/UmumumumApplicationTests.java
+++ src/test/java/in/stonecold/umumumum/UmumumumApplicationTests.java
@@ -1,8 +1,12 @@
 package in.stonecold.umumumum;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it-test")
+@Tag("it-test")
 @SpringBootTest
 class UmumumumApplicationTests {
 
