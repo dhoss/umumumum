4cf2aac07efe011e588ca6a760f1e732e94e9b88
Devin Austin <devin.austin@gmail.com>
2024-07-22 19:08:49 -0600

    initial post handling

---
+++ src/main/java/in/stonecold/umumumum/api/v1/post/PostNode.java
@@ -0,0 +1,17 @@
+package in.stonecold.umumumum.api.v1.post;
+
+import in.stonecold.umumumum.api.v1.PostNodeBuilder;
+import io.soabase.recordbuilder.core.RecordBuilder;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@RecordBuilder
+public record PostNode(
+    Integer id,
+    String title,
+    String body,
+    OffsetDateTime created,
+    OffsetDateTime updated,
+    List<PostNode> children
+) implements PostNodeBuilder.With {}
