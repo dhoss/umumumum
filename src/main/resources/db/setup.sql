57bdc4f1bbfbd4f4a757b0f26d1a43b56de00054
Devin Austin <devin.austin@gmail.com>
2024-08-22 15:01:12 -0600

    added testcontainers dependency, spring data jdbc, database setup sql, local flyway config

---
+++ src/main/resources/db/setup.sql
@@ -0,0 +1,14 @@
+drop database umumumum;
+create database umumumum;
+
+drop owned by umumumum;
+drop user umumumum;
+create user umumumum with password 'umumumum';
+
+grant all privileges on database umumumum to umumumum;
+
+grant all on schema public to umumumum;
+
+grant usage, create on schema public to umumumum;
+
+alter database umumumum owner to umumumum;
